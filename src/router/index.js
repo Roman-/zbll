03b3be4efe81b3df5b230d525ab55ba9a83bca60
Roman Strakhov <roman.strakhov@alberblanc.com>
2023-04-29 14:48:34 +0400

    timer view

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import SelectView from "@/views/SelectView.vue";
 import TimerView from "@/views/TimerView.vue";
 import AboutView from "@/views/AboutView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/select',
--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -4,17 +4,43 @@ import Timer from "@/components/timer/Timer.vue";
 </script>
 
 <template>
-  <div>
-      <Scramble/>
-    <div class="row bg-dark">
+  <div class="d-flex flex-column">
+    <div class="row no-gutters">
+      <div class="col-12 bg-gradient">
+        <Scramble/>
+      </div>
+    </div>
+
+    <div class="row flex-grow-1 bg-gradient">
       <div class="col-8">
         <Timer/>
       </div>
       <div class="col-4">
-        col4
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Stats</h5>
+                <p class="card-text">Some stats here</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">History</h5>
+                <p class="card-text">Some history here</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
+
+
 </template>
 
 <style scoped>
