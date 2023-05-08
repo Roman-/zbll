dbf6ebcdf54f343523ceb3693fa836d6906efbdb
Roman Strakhov <roman.strakhov@alberblanc.com>
2023-05-08 08:04:01 +0400

    presets

--- src/components/presets/Presets.vue
+++ src/components/presets/Presets.vue
@@ -1,12 +1,42 @@
 <script setup>
+import {usePresetsStore} from "@/stores/PresetStore";
+import {onMounted, ref} from "vue";
+import {useSelectedStore} from "@/stores/SelectedStore";
+
+const presets = usePresetsStore()
+const selected = useSelectedStore()
+
+onMounted(() => {
+      presets.set("starred", [])
+      presets.set("abc", [])
+  console.log("map = ", JSON.stringify(presets.map));
+    })
+
+const currentPresetName = ref("")
+
+const saveCurrentPreset = () => {
+  presets.set(currentPresetName.value, selected.allSelectedCases)
+}
+
 </script>
 
 <template>
-  <ul>
-    <li>Preset 1</li>
-    <li>Preset 2</li>
-    <li>Preset 3</li>
-  </ul>
+  <div class="container">
+    <div class="input-group mb-3">
+      <input type="text" v-model="currentPresetName" placeholder="new preset" class="form-control styled" />
+      <button class="btn btn-primary" type="button" id="saveButton">Save</button>
+    </div>
+
+    <div v-for="name in Object.keys(presets.map)" :key="name" class="d-flex align-items-center mb-2">
+      <span class="me-2">{{name}}</span>
+      <button class="btn btn-sm btn-outline-success me-1" type="button">
+        <i class="bi bi-upload"></i>
+      </button>
+      <button class="btn btn-sm btn-outline-danger" type="button">
+        <i class="bi bi-trash"></i>
+      </button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- src/stores/PresetStore.js
+++ src/stores/PresetStore.js
@@ -7,15 +7,17 @@ export const usePresetsStore = defineStore('presets', () => {
   // {name: [array of cases], name: [other array], …}
   const map = reactive(JSON.parse(localStorage.getItem(localStoreKey) || "{}"))
 
+  // set (save) preset
   const set = (name, cases) => {
     map[name] = cases
   }
 
+  // get preset by name
   const get = name => {
     return map[name] ?? []
   }
 
   watch(map, () => localStorage.setItem(localStoreKey, JSON.stringify(map)))
 
-  return { map }
+  return { map, set, get}
 });
